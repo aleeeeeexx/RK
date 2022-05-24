6ed972d3a07b8f7132bfdb4551426a7265fc829a
aleeeeeexx <sasha.fukina.00@mail.ru>
2022-05-24 12:22:51 +0300

    ch

--- server.js
+++ server.js
@@ -11,7 +11,15 @@ http.createServer((request, response) => {
 
 })
 
+let JSON = {
+    Type: get1,
+                Age: get2,
+                poliv:get3,
+                gorsh:get4
 
+    };
+    response.send(JSON);
+    
 
 .listen(5500, () => { console.log("Сервер запущен по адресу http://localhost:5500");
 console.log ("Номер порта 5500")});
