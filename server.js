a5898df5eaf97b8639966626bc2247acd6da71be
aleeeeeexx <sasha.fukina.00@mail.ru>
2022-05-24 12:10:04 +0300

    chhh

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const http = require("http");
 const fs = require("fs");
+const express = require('express');
+const app = express();
 
+
+app.use('/style', express.static(`public`));
 
 http.createServer((request, response) => {
  fs.readFile("index.html", (error, data) => response.end(data));
 
 })
-  
 
 .listen(5500, () => { console.log("Сервер запущен по адресу http://localhost:5500");
 console.log ("Номер порта 5500")});
